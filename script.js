f18e9b55c1db5f5e4a5bd27f1d6f3ea93af5411d
ezemettler <ehmettler@gmail.com>
2025-03-25 22:19:28 -0300

    test 4

--- script.js
+++ script.js
@@ -15,39 +15,38 @@ document.addEventListener("DOMContentLoaded", function () {
     }
 
     function handleSubmit(event, tipoFormulario) {
-        event.preventDefault(); // Evita que la página se recargue
+        event.preventDefault();
 
         const formData = new FormData(event.target);
-        const data = { formulario: tipoFormulario }; // Agregamos el id del formulario como valor
-
+        const data = { formulario: tipoFormulario };
         formData.forEach((value, key) => {
             data[key] = value;
         });
 
-        // Verifica que los datos sean correctos antes de enviarlos
         console.log("Datos enviados al servidor: ", data);
-
-        // Llamada a la función que envía los datos al servidor de Google Apps Script
         sendToGoogleSheets(data);
     }
 
     function sendToGoogleSheets(data) {
-        fetch('https://script.google.com/macros/s/AKfycbwR5AS5XO7U5V5Gmc1iOcjMgs8_SQ_mLD2PKUlYMR8TIe0A8FnHoTvVSuphJ3o3DHRJ/exec', {
-          method: 'POST',
-          body: JSON.stringify(data), // Sigue usando JSON
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          mode: 'cors', // Asegura que se use CORS
-          redirect: 'follow' // Maneja redirecciones si las hay
+        const urlData = new URLSearchParams();
+        for (const key in data) {
+            urlData.append(key, data[key]);
+        }
+
+        fetch('https://script.google.com/macros/s/AKfycbyhm7UrAvs7vKnXJBAZuDAfMl9EDSXlZ5h5EXN9kMAAMLMvJyTjxcMCi2YLTOvqceQb/exec', {
+            method: 'POST',
+            body: urlData,
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
         })
-        .then(response => response.json()) // Cambié a .json() para parsear la respuesta correctamente
+        .then(response => response.json())
         .then(responseData => {
-          alert(responseData.message || "Éxito"); // Muestra el mensaje del servidor
+            alert(responseData.message || "Éxito");
         })
         .catch(error => {
-          console.error('Error:', error);
-          alert('Error: ' + error.message);
+            console.error('Error:', error);
+            alert('Error: ' + error.message);
         });
-      }
+    }
 });
